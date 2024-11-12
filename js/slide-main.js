321eefa3dcebed9384aabb52c6b06397f694452f
barem21 <barem210@gmail.com>
2024-11-12 11:07:13 +0900

    [kakaobrain blog clone project] 메인 비주얼 슬라이드 기기별(PC/모바일) 출력조건 스크립트 작업 진행중

--- js/slide-main.js
+++ js/slide-main.js
@@ -10,10 +10,11 @@ window.addEventListener("load", function () {
     .then(function (result) {
       //console.log(result);
       let slideHtml = "";
+      //result갯수만큼 반복
       for (let i = 0; i < result.length; i++) {
         const obj = result[i];
         const htmlData = `
-          <div class="swiper-slide">
+          <div class="swiper-slide" data-pc="${obj.pc}" data-mobile="${obj.mobile}">
             <a href="${obj.url}">
               <img src="./images/${obj.pic}" alt="${obj.title}" />
               <span class="slide-title">${obj.title}</span>
@@ -29,6 +30,58 @@ window.addEventListener("load", function () {
       );
       mainVisualSlideTag.innerHTML = slideHtml;
 
+      const slideList = document.querySelectorAll(
+        ".visual-slide .swiper-wrapper .swiper-slide"
+      );
+      //console.log(slideList);
+
+      slideList.forEach(function (aaa) {
+        //console.log(aaa);
+        //이후 처리
+      });
+
+      /* ################################################################### */
+
+      //접속환경(PC/모바일) 확인
+      let windowState = "PC"; //초기값은 PC
+      const windowWidth = this.window.innerWidth; //브라우져 넓이 확인
+      //console.log(windowWidth);
+
+      if (windowWidth > 1024) {
+        //pc 버전
+        if (windowState != "PC") {
+          windowState = "PC";
+          //console.log(windowState);
+        }
+      } else {
+        //모바일 버전
+        if (windowState != "Mobile") {
+          windowState = "Mobile";
+          //console.log(windowState);
+        }
+      }
+
+      //브라우저 크기가 바뀌면 접속환경(PC/모바일) 확인
+      window.addEventListener("resize", function () {
+        const windowWidth = this.window.innerWidth;
+
+        if (windowWidth > 1024) {
+          //pc 버전
+          if (windowState != "PC") {
+            windowState = "PC";
+            //console.log(windowState);
+          }
+        } else {
+          //모바일 버전
+          if (windowState != "Mobile") {
+            windowState = "Mobile";
+            //console.log(windowState);
+          }
+        }
+      });
+
+      /* ################################################################### */
+
       //슬라이드 처리
       const visualSwiper = new Swiper(".visual-slide", {
         loop: true,
@@ -42,6 +95,8 @@ window.addEventListener("load", function () {
         },
         speed: 800,
       });
+
+      /* ################################################################### */
 
       //마우스 오버/아웃에 따른 자동재생 정지/시작 처리 추가
       const visualSwiperSelect = document.querySelector(".visual-slide");
